07b4ed6bb252444786dc2e7fb0f726b28c1f3f9f
Perry <perrymacdougall@gmail.com>
2019-06-03 18:03:55 +0000

    stored chunks into a buffer variable

--- transformToHTML.js
+++ transformToHTML.js
@@ -4,21 +4,33 @@ function getAndPrintHTMLChunks () {
   // Setting host and filepath info
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
+
 
   // Callback function that receives output from request
   var callback = function(response) {
-    // Response handler function that modifies output
+
+    // Defining an array to store the chunks
+    var output = "";
+
+    // Response handler function that pushes the chunks into buffer
     response.on('data', function(chunk) {
-      // Prints the output as a string
-      console.log(chunk.toString() + '\n');
-    })
+      var buffer = chunk.toString();
+      output += buffer;
+    });
+
+    response.on('end', function() {
+      console.log(output);
+      console.log('Finished responding');
+    });
   }
+
 
   // Makes the request, passing in the destination info and callback handler as arguments
   // end() method is very important!! I'm guessing it ends the request, like hanging up a telephone
   https.get(requestOptions, callback);
+
 
 }
 
