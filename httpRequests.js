aa09f5f7ddb1465eba93135d6c3d7213b03bedda
Perry <perrymacdougall@gmail.com>
2019-06-03 16:49:49 +0000

    replaced request with get as per exercise instructions

--- httpRequests.js
+++ httpRequests.js
@@ -1,23 +1,25 @@
 var https = require('https');
 
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step1.html'
-};
+function getAndPrintHTMLChunks () {
+  // Setting host and filepath info
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
 
-  function getAndPrintHTMLChunks () {
-
-
-    // Callback function that receives output from request
-    var callback = function(response) {
-      // Response handler function that modifies output
-      response.on('data', function(chunk) {
-        console.log(chunk.toString() + '\n');
-      })
-    }
-
-    https.request(requestOptions, callback).end();
-
+  // Callback function that receives output from request
+  var callback = function(response) {
+    // Response handler function that modifies output
+    response.on('data', function(chunk) {
+      // Prints the output as a string
+      console.log(chunk.toString() + '\n');
+    })
   }
+
+  // Makes the request, passing in the destination info and callback handler as arguments
+  // end() method is very important!! I'm guessing it ends the request, like hanging up a telephone
+  https.get(requestOptions, callback).end();
+
+}
 
 getAndPrintHTMLChunks();
